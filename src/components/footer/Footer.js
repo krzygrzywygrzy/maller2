83f25db0f896c2202fc146017fcbd08208ffdfe4
trikitirem <trikitirem@outlook.com>
2022-03-05 17:52:32 +0100

    footer proper positioning

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,6 @@ const Footer = () => {
         <span>
           Attention! This is not real shop! Orders won't be executed!{" "}
         </span>
-        <br />
         <span>All rights belong to their rightful owners! </span>
       </div>
     </footer>
