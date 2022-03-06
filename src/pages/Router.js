fc428b96efe651f00b83003655590515e091c59b
trikitirem <trikitirem@outlook.com>
2022-03-06 19:18:48 +0100

    bux fix attempt 2

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "../components/navbar/Navbar";
 import { Route } from "wouter";
 import Home from "./home/Home";
 import Cart from "./cart/Cart";
